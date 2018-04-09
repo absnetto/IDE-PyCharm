69e9f321f260f1b3fd48b284c5b42c1270011863
absnetto <abs.netto@gmail.com>
2018-04-09 09:25:47 -0300

    resolvido.

--- URI/Criptografia 1024.py
+++ URI/Criptografia 1024.py
@@ -22,19 +22,30 @@ def passada2(texto):
 
 
 def passada3(texto):
-    return texto
+    tamanho = (len(texto))
+    meio = tamanho//2
+    texto1 = texto[0:meio]
+    texto2 = texto[meio:tamanho] #meio+1 aqui no no item anterior...
+    novotexto = ""
+    for s in texto2:
+        s = chr(ord(s)-1)
+        novotexto += s
+
+    return texto1+novotexto
 
 
 N = int(input())
 qtde = 0
 lista = []
 while qtde < N:
-    lista.append(input())
-    qtde+=1
-
+    if (1 <= N <= (10**4)):
+        lista.append(input())
+        qtde+=1
+    else:
+        break
 
 for l in lista:
     l = passada1(l)
     l = passada2(l)
-    #x = passada3(l)
+    l = passada3(l)
     print(l)
